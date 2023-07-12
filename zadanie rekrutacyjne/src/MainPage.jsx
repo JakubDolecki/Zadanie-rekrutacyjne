19fb0b24235d70e467565f1cc7a69e9d065ddd6b
JakubDolecki <jakubdolecki1@wp.pl>
2023-07-12 12:26:16 +0200

    added function that fetches data

--- zadanie rekrutacyjne/src/MainPage.jsx
+++ zadanie rekrutacyjne/src/MainPage.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export function MainPage() {
-  return (<span>witam</span>);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    const getData = async () => {
+      const url = "https://jsonplaceholder.typicode.com/posts/";
+      try {
+        const resp = await fetch(url);
+        const data = await resp.json();
+        setData(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getData();
+
+  }, []);
+  
+  console.log(data);
+
+  return <span>witam</span>;
 }
